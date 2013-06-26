7e12ebe21b2f2b187af57ea2cd1dae6cd8108fb0
x3igh7 <connerpsmith@gmail.com>
2013-06-26 17:26:14 -0400

    added uniqueness for users and team memberships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130625212318) do
+ActiveRecord::Schema.define(:version => 20130626205914) do
 
   create_table "memberships", :force => true do |t|
     t.integer "user_id",                       :null => false
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(:version => 20130625212318) do
     t.string  "role",    :default => "member", :null => false
     t.boolean "active",  :default => false,    :null => false
   end
+
+  add_index "memberships", ["team_id"], :name => "index_memberships_on_team_id"
+  add_index "memberships", ["user_id", "team_id"], :name => "index_memberships_on_user_id_and_team_id", :unique => true
+  add_index "memberships", ["user_id"], :name => "index_memberships_on_user_id"
 
   create_table "teams", :force => true do |t|
     t.string "name", :null => false
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -9,6 +9,16 @@ describe Membership do
   it { should validate_presence_of :role }
 
   it { should ensure_inclusion_of(:role).in_array(%w(owner member)) }
+  
+  describe "should have 1 membership per user per team" do
+    team = FactoryGirl.build(:team)
+    user = FactoryGirl.build(:user)
+    before do
+      FactoryGirl.create(:membership, team: team, role: 'member')
+    end
+    
+    it { should validate_uniqueness_of(:user_id) }
+  end
 
   describe "should ensure the inclusion of active" do
     it { should have_valid(:active).when(true)}
