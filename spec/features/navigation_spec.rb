95b3cbf6c64f378eb6ad4c7b46f4c1598345fda3
Raphael-Joel Lim <kgerbil@gmail.com>
2013-07-23 12:32:05 +0900

    minor layout/css tweaks

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -8,22 +8,22 @@ describe "Navigation" do
     end
 
     it "links to the home page" do
-      page.find('.nav').click_on "home"
+      page.find('#nav-main').click_on "home"
       expect(current_path).to eq(root_path)
     end
   
     it "links to memebers index" do
-      page.find('.nav').click_on "members"
+      page.find('#nav-main').click_on "members"
       expect(current_path).to eq(user_index_path)
     end
   
     it "links to teams index" do
-      page.find('.nav').click_on "teams"
+      page.find('#nav-main').click_on "teams"
       expect(current_path).to eq(teams_path)
     end
   
     it "links to tournaments index" do
-      page.find('.nav').click_on "tournaments"
+      page.find('#nav-main').click_on "tournaments"
       expect(current_path).to eq(tournaments_path)
     end
 
