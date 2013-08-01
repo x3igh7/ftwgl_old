72fccd2e02632cf1885f94cd8a09aa7aaf62d8d5
Raphael-Joel Lim <kgerbil@gmail.com>
2013-08-01 16:20:41 +0900

    users paginated in admin cpanel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,16 @@
 class Admin::UsersController < AdminController
+	PAGESIZE = 5
+	
 	def index
-		@users = User.find(:all)
-		@response = [] 
+		page = params[:page].to_i
+		#extremely inefficient pagination
+		@users = User.order(:username)[ (page - 1) * PAGESIZE, PAGESIZE ]
+		@response = {}
+		@response[:users] = []
 		@users.each do |user| 
-			@response << { :id => user.id, :username => user.username, :email => user.email, :banned => user.banned? }
+			@response[:users] << { :id => user.id, :username => user.username, :email => user.email, :banned => user.banned? }
 		end
+		@response[:total_pages] = ( ( User.count - 1) / PAGESIZE ).floor + 1
 		render :json => @response
 		return
  	end
