05a20f9be90195487a06b0bde0ac7207595e7a2f
Conner Smith <connerpsmith@gmail.com>
2013-09-30 21:20:51 -0400

    refactored all tests

--- spec/features/admincp_spec.rb
+++ spec/features/admincp_spec.rb
@@ -6,6 +6,11 @@ describe "Admin CP" do
   let!(:tournament2) {FactoryGirl.create(:tournament, active: false)}
   let!(:admin) {FactoryGirl.create(:user)}
 
+  it "cannot access admincp if not admin" do
+    visit admin_root_path
+    expect(current_path).to eq(root_path)
+  end
+
   context "Tournament Management" do
 
     before do
@@ -20,34 +25,36 @@ describe "Admin CP" do
       expect(page).to_not have_content(tournament2.name)
     end
 
-    it "has an option for editting tournament info", :js => true do
-      visit admin_root_path
-      expect(page).to have_content(tournament1.name)
-      click_button "manage"
-      expect(page).to have_content("edit")
-      expect(page).to have_content("teams")
-      expect(page).to have_content("matches")
-      expect(page).to have_content("rankings")
-      expect(page).to have_content("schedule")
-      expect(page).to have_content("hide")
-    end
+    context "edit tournament" do
+      it "has an option for editting tournament info", :js => true do
+        visit admin_root_path
+        expect(page).to have_content(tournament1.name)
+        click_button "manage"
+        expect(page).to have_content("edit")
+        expect(page).to have_content("teams")
+        expect(page).to have_content("matches")
+        expect(page).to have_content("rankings")
+        expect(page).to have_content("schedule")
+        expect(page).to have_content("hide")
+      end
 
-    it "links to tournament edit", :js => true do
-      visit admin_root_path
-      click_button "manage"
-      click_link "edit"
-      expect(page).to have_content("edit tournament")
-      expect(page).to have_button("update tournament")
-    end
+      it "links to tournament edit", :js => true do
+        visit admin_root_path
+        click_button "manage"
+        click_link "edit"
+        expect(page).to have_content("edit tournament")
+        expect(page).to have_button("update tournament")
+      end
 
-    it "successfully updates a tournament", :js => true, :focus => true do
-      visit admin_root_path
-      click_button "manage"
-      click_link "edit"
-      fill_in "Name", with: "FTW Season 3"
-      click_button "update tournament"
-      expect(current_path).to eq(admin_root_path)
-      expect(page).to have_content("FTW Season 3")
+      it "successfully updates a tournament", :js => true do
+        visit admin_root_path
+        click_button "manage"
+        click_link "edit"
+        fill_in "Name", with: "FTW Killfest 3"
+        click_button "update tournament"
+        expect(current_path).to eq(admin_root_path)
+        expect(page).to have_content("FTW Killfest 3")
+      end
     end
 
     it "shows tournament teams" do
