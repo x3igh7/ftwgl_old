84e30d9ec7315c1c5cade9e28bce85462b37ed42
Conner Smith <connerpsmith@gmail.com>
2013-11-25 17:07:32 -0600

    added test for has_played? method - passed

--- spec/models/tournament_team_spec.rb
+++ spec/models/tournament_team_spec.rb
@@ -14,7 +14,7 @@ describe TournamentTeam do
     before do
       FactoryGirl.create(:tournament_team, team: team, tournament: tournament)
     end
-  
+
     it { should validate_uniqueness_of(:team_id).scoped_to(:tournament_id) }
   end
 
@@ -27,7 +27,7 @@ describe TournamentTeam do
     let!(:team1) { FactoryGirl.create(:tournament_team, team: home, tournament: tournament) }
     let!(:team2) { FactoryGirl.create(:tournament_team, team: away, tournament: tournament) }
     let!(:match) { FactoryGirl.create(:match, home_team_id: team1.id, away_team_id: team2.id, tournament_id: tournament.id, home_score: 10, away_score: 5, winner_id: team1.id) }
-    
+
     it "calc_diff" do
       home_team = match.home_team
       expect(home_team.calc_diff(match)).to eq(5)
@@ -47,5 +47,24 @@ describe TournamentTeam do
       expect(home_team.total_points).to eq(0)
     end
 
+  end
+
+  describe "has_not_played" do
+    let!(:tournament1) {FactoryGirl.create(:tournament)}
+    let!(:team) { FactoryGirl.create(:team) }
+    let!(:team2) { FactoryGirl.create(:team) }
+    let!(:team3) { FactoryGirl.create(:team) }
+    let!(:tournament_team) {FactoryGirl.create(:tournament_team, team: team, tournament: tournament1, rank: 6)}
+    let!(:tournament_team2) {FactoryGirl.create(:tournament_team, team: team2, tournament: tournament1, rank: 5)}
+    let!(:tournament_team3) {FactoryGirl.create(:tournament_team, team: team3, tournament: tournament1, rank: 4)}
+    let!(:match) { FactoryGirl.create(:match, home_team_id: tournament_team.id, away_team_id: tournament_team2.id, tournament_id: tournament1.id) }
+
+    it "has_played? method checks if a tournament team has already played a specified tournament team", :focus => true do
+      check = tournament_team.has_played?(team2)
+      expect(check).to be_true
+    end
+
+    it "has_not_played returns an array of teams not yet played yet, ordered by rank" do
+    end
   end
 end
