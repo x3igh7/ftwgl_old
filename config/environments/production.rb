1f82e1c2b0cd383a4f054a4f7b8a1bb1af095ab4
Conner Smith <connerpsmith@gmail.com>
2014-01-16 23:06:10 -0600

    updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Ftwgl::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.js_compressor = :uglifier
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
