889ca2eb31505b4aa86326bf6d18c8d35e859451
Conner Smith <connerpsmith@gmail.com>
2014-01-16 23:15:25 -0600

    updates

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Ftwgl
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.0'config.assets.initialize_on_precompile = false
     config.assets.initialize_on_precompile = true
   end
 end
