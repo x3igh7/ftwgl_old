e983102a2be7fd5d3d3c6461c8bc21ee3b4ffd31
Conner Smith <connerpsmith@gmail.com>
2014-02-25 12:52:17 -0600

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-datepicker
-//= require new_tournament.js
-//= require show_tournament.js
-//= require add_tournament_team.js
-//= require jquery.challonge.js
 
 $(document).ready(function(){
   $(".management").hide();
--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,5 @@ module Ftwgl
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
-
-    config.assets.compile = true
     end
 end
