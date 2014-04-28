27b237f89e6493f323b194514002fae9720a1cb7
Conner Smith <connerpsmith@gmail.com>
2014-04-28 15:04:41 -0500

    working on next feature test for playoffs

--- spec/features/admincp_add_playoffs_spec.rb
+++ spec/features/admincp_add_playoffs_spec.rb
@@ -5,8 +5,12 @@ describe "Playoffs creation" do
   let!(:tournament) {FactoryGirl.create(:tournament)}
   let!(:team) { FactoryGirl.create(:team) }
   let!(:team2) { FactoryGirl.create(:team) }
-  let!(:tournament_team) {FactoryGirl.create(:tournament_team, team: team, tournament: tournament)}
-  let!(:tournament_team2) {FactoryGirl.create(:tournament_team, team: team2, tournament: tournament)}
+  let!(:team3) { FactoryGirl.create(:team) }
+  let!(:team4) { FactoryGirl.create(:team) }
+  let!(:tournament_team) {FactoryGirl.create(:tournament_team, team: team, tournament: tournament, challonge_id: 12611718)}
+  let!(:tournament_team2) {FactoryGirl.create(:tournament_team, team: team2, tournament: tournament, challonge_id: 12611728)}
+  let!(:tournament_team3) {FactoryGirl.create(:tournament_team, team: team3, tournament: tournament, challonge_id: 12611723)}
+  let!(:tournament_team4) {FactoryGirl.create(:tournament_team, team: team4, tournament: tournament, challonge_id: 12611725)}
 
   before do
     admin.roles = :admin
@@ -14,18 +18,27 @@ describe "Playoffs creation" do
     sign_in_as(admin)
   end
 
-  it 'creates playoff bracket for a season tournament', :vcr => {:record => :new_episodes}, :js => true, :focus => true do
+  it 'creates playoff bracket for a season tournament', :vcr => {:record => :new_episodes}, :js => true do
     manage
     click_on 'start playoffs'
     expect(page).to have_content('generate playoff bracket')
-    fill_in "bracket_size", with: 2
+    fill_in "bracket_size", with: 4
     click_on 'show playoff teams'
     expect(page).to have_content(team.name)
     expect(page).to have_content(team2.name)
     click_on 'start playoffs'
     playoff_tournament = Tournament.find(tournament.id)
-    expect(playoff_tournament.bracket_size).to eq(2)
+    expect(playoff_tournament.bracket_size).to eq(4)
     expect(playoff_tournament.playoffs).to be_true
+  end
+
+  it 'generates playoff matches so that matches can be recorded', :vcr => {:record => :new_episodes}, :js => true, :focus => true do
+    prev = Match.count
+    manage
+    click_link "generate matches"
+    click_on 'generate playoff matches'
+    expect(Match.count).to eq(prev + 2)
+    expect(Match.last.challonge_id).to eq(18031474)
   end
 
 end
