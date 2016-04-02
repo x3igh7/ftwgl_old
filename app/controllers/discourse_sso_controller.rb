cad604536ce10d8c310ae63ef0c14edf47def1ef
Conner Smith <connerpsmith@gmail.com>
2016-04-02 14:38:35 -0500

    removed full name property assignment

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -6,11 +6,10 @@ class DiscourseSsoController < ApplicationController
   def sso
     sso = SingleSignOn.parse(request.query_string, ENV["SSO_SECRET"])
     sso.email = current_user.email # from devise
-    sso.name = current_user.full_name # this is a custom method on the User class
     sso.username = current_user.username # from devise
     sso.external_id = current_user.id # from devise
     sso.sso_secret = ENV["SSO_SECRET"]
 
-    redirect_to sso.to_url(ENV["FORUMS_URL"] + "/session/sso_login")
+    redirect_to sso.to_url("http://forums.ftwgl.com/session/sso_login")
   end
 end
