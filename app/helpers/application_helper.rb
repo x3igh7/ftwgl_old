2799bb396f63b74eb8142e89d4bf0393c36a2f0e
Conner Smith <connerpsmith@gmail.com>
2016-04-04 19:49:41 -0500

    fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,33 +11,45 @@ module ApplicationHelper
 
   def big_avatar_url(user)
     default_url = asset_url("guest_big.png")
-    if user.gravatar_email == "" || user.gravatar_email.nil?
-      return "guest_big.png"
+    unless user.nil?
+      if user.gravatar_email == ""
+        return "guest_big.png"
+      else
+        gravatar_id = Digest::MD5::hexdigest(user.gravatar_email).downcase
+        return "http://gravatar.com/avatar/#{gravatar_id}.png?s=128&d=#{CGI.escape(default_url)}"
+      end
     else
-      gravatar_id = Digest::MD5::hexdigest(user.gravatar_email).downcase
-      return "http://gravatar.com/avatar/#{gravatar_id}.png?s=128&d=#{CGI.escape(default_url)}"
+      return "guest_big.png"
     end
   end
 
   def team_avatar_url(team)
     default_url = asset_url("default_team.jpg")
 
-    if team.gravatar_email == "" || team.gravatar_email.nil?
-      return "default_team.jpg"
+    unless team.nil?
+      if team.gravatar_email == ""
+        return "default_team.jpg"
+      else
+        gravatar_id = Digest::MD5::hexdigest(team.gravatar_email).downcase
+        return "http://gravatar.com/avatar/#{gravatar_id}.png?s=256&d=#{CGI.escape(default_url)}"
+      end
     else
-      gravatar_id = Digest::MD5::hexdigest(team.gravatar_email).downcase
-      return "http://gravatar.com/avatar/#{gravatar_id}.png?s=256&d=#{CGI.escape(default_url)}"
+      return "default_team.jpg"
     end
   end
 
   def small_team_avatar_url(team)
     default_url = asset_url("default_team_small.jpg")
 
-    if team.gravatar_email == ""
-      return "default_team_small.jpg"
+    unless team.nil?
+      if team.gravatar_email == ""
+        return "default_team_small.jpg"
+      else
+        gravatar_id = Digest::MD5::hexdigest(team.gravatar_email).downcase
+        return "http://gravatar.com/avatar/#{gravatar_id}.png?s=32&d=#{CGI.escape(default_url)}"
+      end
     else
-      gravatar_id = Digest::MD5::hexdigest(team.gravatar_email).downcase
-      return "http://gravatar.com/avatar/#{gravatar_id}.png?s=32&d=#{CGI.escape(default_url)}"
+      return "default_team_small.jpg"
     end
   end
 
