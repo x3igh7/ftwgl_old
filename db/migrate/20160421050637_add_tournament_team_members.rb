ad31f759b2915f86a6f0e9d227a5f3ad2e84911b
Conner Smith <connerpsmith@gmail.com>
2016-04-21 00:17:53 -0500

    updated migration. create model

--- db/migrate/20160421050637_add_tournament_team_members.rb
+++ db/migrate/20160421050637_add_tournament_team_members.rb
@@ -1,6 +1,7 @@
 class AddTournamentTeamMembers < ActiveRecord::Migration
   def change
   	create_table :tournament_team_membership do |t|
+  		t.integer :tournament_id, null: false
   		t.integer :tournament_team_id, null: false
   		t.integer :user_id, null: false
   		t.string :authorization_id
@@ -8,7 +9,7 @@ class AddTournamentTeamMembers < ActiveRecord::Migration
   		t.timestamps
   	end
 
-  	add_index :tournament_team_membership, [:user_id], unique: true
+  	add_index :tournament_team_membership, [:tournament_id, :user_id], unique: true
   	add_index :tournament_team_membership, [:tournament_team_id]
   end
 end
