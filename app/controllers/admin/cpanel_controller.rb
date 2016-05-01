ce1fb9b5be5944468fe84863c6b01ad22acf9698
Conner Smith <connerpsmith@gmail.com>
2016-05-01 13:48:01 -0500

    covert to spaces

--- app/controllers/admin/cpanel_controller.rb
+++ app/controllers/admin/cpanel_controller.rb
@@ -7,93 +7,93 @@ class Admin::CpanelController < AdminController
     @admin_news = nil
 
     if @user.is_tournament_admin?
-    	@active_tournaments = @user.admin_tournaments
-    	@admin_teams = @user.admin_teams
-    	@admin_news = @user.admin_news
+      @active_tournaments = @user.admin_tournaments
+      @admin_teams = @user.admin_teams
+      @admin_news = @user.admin_news
     else
- 	    @active_tournaments = Tournament.where("active = true")
- 	  end
+      @active_tournaments = Tournament.where("active = true")
+    end
 
- 	  @q_users = User.search(params[:q])
-	  @q_teams = Team.search(params[:q])
-	  @q_news = News.search(params[:q])
+    @q_users = User.search(params[:q])
+    @q_teams = Team.search(params[:q])
+    @q_news = News.search(params[:q])
 
-	  if(params[:q] != nil)
+    if(params[:q] != nil)
 
-		  if(params[:q][:username_or_email_cont] != nil)
-		  	@users = @q_users.result.order("username").page params[:page_1]
-			else
-				@users = User.order("username").page params[:page_1]
-			end
+    if(params[:q][:username_or_email_cont] != nil)
+      @users = @q_users.result.order("username").page params[:page_1]
+      else
+        @users = User.order("username").page params[:page_1]
+      end
 
-			if(params[:q][:name_or_tag_cont] != nil)
-		  	if @user.is_tournament_admin?
-		  		@teams = @q_teams.result.order("name")
+      if(params[:q][:name_or_tag_cont] != nil)
+        if @user.is_tournament_admin?
+          @teams = @q_teams.result.order("name")
 
-		  		@teams.each_with_index do |t, i|
-		  			if !@admin_teams.include?(t)
-		  				@teams.delete_at(i)
-		  			end
-		  		end
+          @teams.each_with_index do |t, i|
+            if !@admin_teams.include?(t)
+              @teams.delete_at(i)
+            end
+          end
 
-		  		@teams = @teams.page params[:page_2]
-		  	else
-					@teams = @q_teams.result.order("name").page params[:page_2]
-				end
-			else
-				if @user.is_tournament_admin?
-					@teams = @admin_teams
-				else
-					@teams = Team.order("name").page params[:page_2]
-				end
-			end
+          @teams = @teams.page params[:page_2]
+        else
+          @teams = @q_teams.result.order("name").page params[:page_2]
+        end
+      else
+        if @user.is_tournament_admin?
+          @teams = @admin_teams
+        else
+          @teams = Team.order("name").page params[:page_2]
+        end
+      end
 
-			if(params[:q][:headline_cont] != nil)
-		  	if @user.is_tournament_admin?
-		  		@news = @q_news.result.order("created_at")
+      if(params[:q][:headline_cont] != nil)
+        if @user.is_tournament_admin?
+          @news = @q_news.result.order("created_at")
 
-		  		@news.each_with_index do |n, i|
-			  		if !@admin_news.include?(n)
-			  			@news.delete_at(i)
-			  		end
-		  		end
+          @news.each_with_index do |n, i|
+            if !@admin_news.include?(n)
+              @news.delete_at(i)
+            end
+          end
 
-		  		@news = @news.page params[:page_3]
-		  	else
-		  		@news = @q_news.result.order("created_at").page params[:page_3]
-		  	end
-		  else
-		  	if @user.is_tournament_admin?
-					@news = @admin_news
-				else
-		  		@news = News.order("created_at").page params[:page_3]
-		  	end
-		  end
+          @news = @news.page params[:page_3]
+        else
+          @news = @q_news.result.order("created_at").page params[:page_3]
+        end
+      else
+        if @user.is_tournament_admin?
+          @news = @admin_news
+        else
+          @news = News.order("created_at").page params[:page_3]
+        end
+      end
 
-		else
-			@users = User.order("username").page params[:page_1]
-			@teams = Team.order("name")
-			@news = News.order("created_at")
+    else
+      @users = User.order("username").page params[:page_1]
+      @teams = Team.order("name")
+      @news = News.order("created_at")
 
-			if @user.is_tournament_admin?
+      if @user.is_tournament_admin?
 
-				@news.each_with_index do |n, i|
-		  		if !@admin_news.include?(n)
-		  			@news.delete_at(i)
-		  		end
-		  	end
+        @news.each_with_index do |n, i|
+          if !@admin_news.include?(n)
+            @news.delete_at(i)
+          end
+        end
 
-		  	@teams.each_with_index do |t, i|
-		  		if !@admin_teams.include?(t)
-		  			@teams.delete_at(i)
-		  		end
-		  	end
+        @teams.each_with_index do |t, i|
+          if !@admin_teams.include?(t)
+            @teams.delete_at(i)
+          end
+        end
 
-			end
+      end
 
-			@news = @news.page params[:page_3]
-		  @teams = @teams.page params[:page_2]
-		end
+      @news = @news.page params[:page_3]
+      @teams = @teams.page params[:page_2]
+    end
   end
 
 end
