7e68604513a233bef69942480c9a35f7f004156d
Conner Smith <connerpsmith@gmail.com>
2016-05-09 22:33:54 -0500

    fix user profiles

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,6 @@ class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
     @prev_matches = @user.matches.where('winner_id IS NOT NULL').limit(5).order(:created_at)
-    @wins = @user.total_wins
-    @losses = @user.total_losses
-    @winning_perc = @user.winning_perc
     @comments = @user.comments.limit(5)
     @upcoming_match = @user.matches.where('winner_id IS NULL').limit(5).order(:created_at)
   end
