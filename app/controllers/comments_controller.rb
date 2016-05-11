4a22f8ce871cabb4f564fe342e988838c6c6dfab
Conner Smith <connerpsmith@gmail.com>
2016-05-11 09:10:45 -0500

    fixing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
       @news = News.find(params[:news_id])
       @comment = @news.comments.build(:content => params[:comment][:content], :user_id => @user.id )
       if @comment.save
-        flash[:notice] = "Comment accepted"
+        flash[:notice] = 'Comment accepted'
         redirect_to :back
       else
         flash[:alert] = @comment.errors.full_messages.first
@@ -17,15 +17,13 @@ class CommentsController < ApplicationController
       @match = Match.find(params[:match_id])
       @home_team = @match.home_team.team
       @away_team = @match.away_team.team
-      if current_user.is_team_member?(@home_team) or current_user.is_team_member?(@away_team)
-        @comment = @match.comments.build( :content => params[:comment][:content], :user_id => current_user.id )
-        if @comment.save
-          flash[:notice] = 'Comment accepted'
-          redirect_to :back
-        else
-          flash[:alert] = @comment.errors.full_messages.first
-          redirect_to tournament_match_path(@match)
-        end
+      @comment = @match.comments.build( :content => params[:comment][:content], :user_id => current_user.id )
+      if @comment.save
+        flash[:notice] = 'Comment accepted'
+        redirect_to :back
+      else
+        flash[:alert] = @comment.errors.full_messages.first
+        redirect_to tournament_match_path(@match)
       end
     end
   end
